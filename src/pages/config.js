cd539f664000e1113b428ee97a8770b9769f1b96
kammara-keshava <kesava9347@gmail.com>
2025-11-05 10:27:36 +0530

    modified files

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,2 +1,2 @@
-export const appId=250721041;
-export const serverSecret="4b4e4f2314ef369eaa7ee6e99e8de322";
+export const appId=yourappidnumber;
+export const serverSecret="yourserversecretidentifiernumber";
